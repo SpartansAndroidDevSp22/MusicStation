85047a98be016f174ca71dc3c22f6858d82db32f
Truc Phan <pttruc.308@gmail.com>
2022-04-15 11:36:50 -0700

    add fragments

--- app/src/main/java/com/example/musicstation/MainActivity.kt
+++ app/src/main/java/com/example/musicstation/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.musicstation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.musicstation.fragments.DiscoverFragment
+import com.example.musicstation.fragments.ProfileFragment
+import com.example.musicstation.fragments.SearchFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentManager: FragmentManager = supportFragmentManager
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnItemSelectedListener {
+                item ->
+            var fragmentToShow: Fragment? = null
+            when (item.itemId) {
+                R.id.action_discover -> {
+                    fragmentToShow = DiscoverFragment()
+                }
+                R.id.action_search -> {
+                    fragmentToShow = SearchFragment()
+                }
+                R.id.action_profile -> {
+                    fragmentToShow = ProfileFragment()
+                }
+            }
+
+            if (fragmentToShow != null) {
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragmentToShow).commit()
+            }
+
+            //return true to say that we've handled this user interaction on the item
+            true
+        }
+
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).selectedItemId = R.id.action_discover
     }
 }
